e2573157ac47695539a44099cc07ff322bcc575d
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-11-26 20:55:21 +0100

    Renversement ordre des fonctions #5 + formatage PEP8
    2 lignes entre blocs de fonctions
    1 ligne entre fonctions du même bloc
    1 ligne après commentaires généraux
    0 après nommage de bloc de fonctions

--- simulation_conso_france.py
+++ simulation_conso_france.py
@@ -2,8 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-'''
-Comment ça fonctionne ?
+'''Comment ça fonctionne ?
  - on calcule en premier lieu les consommations estimées (connu)
  - on lance la simulation :
    - il y a un champ d'année courante
@@ -15,12 +14,12 @@ Comment ça fonctionne ?
 '''
 
 
-'''
-CONSTANTES
-'''
+'''CONSTANTES'''
+
 ANNEE_DEBUT = 2018
 ANNEE_FIN = 2050
 ANNEES = ANNEE_FIN - ANNEE_DEBUT
+
 
 NB_DONNEES = 11
 
@@ -41,6 +40,7 @@ POURCENTAGE_PERDU_DEF_RAFFINEMENT_RAPP_CONSO = 1.75
 POURCENTAGE_PERDU_DEF_SEMI_FINISHED_RAPP_CONSO = 1.09
 POURCENTAGE_NEW_WASTE_RAPP_CONSO = 16.4
 
+
 TEMPS_VEHICULES = 5
 TEMPS_BAT = 50
 TEMPS_EQUIP_ELEC = 10
@@ -48,80 +48,137 @@ TEMPS_APP_ELEC = 3
 
 annee_actuelle = ANNEE_DEBUT
 
-'''
-TABLEAU
+'''TABLEAU
 Enregistre toutes les données, année après années.
 Système d'accès au tableau :
 - l'indice de colonne correspond à année courante - année de départ
 - la colonne correspond à la donnée intéressante suivant le code des constantes
 
 '''
+
 resultats = np.zeros( (NB_DONNEES, ANNEES) )
 
 
-# Programme principal
+## Accesseurs partie production (tout est initialisé avant, on peut y accéder sans pb)
+
+# getConsoTotale
+def getConsoVehicules(annee):
+    return resultats[LIGNE_CONSO_VEHICULES, annee_actuelle - ANNEE_DEBUT]
+
+def getConsoBat(annee):
+    return resultats[LIGNE_CONSO_BAT, annee_actuelle - ANNEE_DEBUT]
+
+def getConsoEquipElec(annee):
+    return resultats[LIGNE_CONSO_EQUIP_ELEC, annee_actuelle - ANNEE_DEBUT]
+
+def getConsoAppElec(annee):
+    return resultats[LIGNE_CONSO_APP_ELEC, annee_actuelle - ANNEE_DEBUT]
+
+def getConsoTotale(annee):
+    return getConsoVehicules(annee) + getConsoBat(annee) + getConsoEquipElec(annee) + getConsoAppElec(annee)
+
+
+# getPerduProductionTotal
+def getPerduProductionRaffinement(annee):
+    '''Donne ce qui est perdu définitivement lors de la partie raffinement de la production'''
+    return resultats[LIGNE_PERDU_PROD_RAFF, annee_actuelle - ANNEE_DEBUT]
+
+def getPerduProductionSemiFinished(annee):
+    '''Donne ce qui est perdu définitivement lors de la production des semi-finished goods'''
+    return resultats[LIGNE_PERDU_PROD_SEMI_FINISHED, annee_actuelle - ANNEE_DEBUT]
+
+def getPerduProductionTotal(annee):
+    '''Donne ce qui est perdu définitivement lors de la production (raffinement + semi-finished goods)'''
+    return getPerduProductionRaffinement(annee) + getPerduProductionSemiFinished(annee)
+
+
+# getRecyclagePrimaire
+def getRecyclagePrimaire(annee):
+    '''Donne ce qui part en recyclage primaire (new waste)'''
+    return resultats[LIGNE_RECYCLAGE_PRIMAIRE, annee_actuelle - ANNEE_DEBUT]
+
+
+# getBesoin
+def getBesoin(annee):
+    '''Donne le besoin du pays pour l'année, en intégrant à la fois
+    - ce qui sera consommé
+    - ce qui sera perdu définitivement (raffinement + semi-finished goods)
+    - ce qui partira en recyclage primaire (new-waste)
+    Ca correspond à ce qui rentre dans le processus de production
+    '''
+    return getConsoTotale(annee) + getPerduProductionTotal(annee) + getRecyclagePrimaire(annee)
+
+
+## Accesseurs partie stock (il faut prendre garde à avoir déjà calculé la valeur correspondante !)
+
+def getStockVehicules(annee):
+    return resultats[LIGNE_STOCK_VEHICULES, annee_actuelle - ANNEE_DEBUT]
+
+def getStockBat(annee):
+    return resultats[LIGNE_STOCK_BAT, annee_actuelle - ANNEE_DEBUT]
+
+def getStockEquipElec(annee):
+    return resultats[LIGNE_STOCK_EQUIP_ELEC, annee_actuelle - ANNEE_DEBUT]
+
+def getStockAppElec(annee):
+    return resultats[LIGNE_STOCK_APP_ELEC, annee_actuelle - ANNEE_DEBUT]
+
+def getStock(annee):
+    return getStockVehicules(annee) + getStockBat(annee) + getStockEquipElec(annee) + getStockAppElec(annee)
+
+
+## Accesseurs partie après le stock (à TODO) :
+
+'''Donne ce qui sort des stocks:
+- ce qui va être recyclé (fin de vie, old-waste), somme de toutes les catégories
+- ce qui va être perdu définitivement (abandonné, non-recyclé et perdu lors du recyclage)
 '''
-Fait tout
-'''
-def simul():
-    initialiser()
-    while annee_actuelle < ANNEE_FIN:
-        doAnneeSuivante()
-    #TODO
+# getSortieStock
+def getSortieStockVehicules(annee):
+    return resultats[LIGNE_CONSO_VEHICULES, annee - TEMPS_VEHICULES]
+
+def getSortieStockBat(annee):
+    return resultats[LIGNE_CONSO_BAT, annee - TEMPS_BAT]
+
+def getSortieStockEquipElec(annee):
+    return resultats[LIGNE_CONSO_EQUIP_ELEC, annee - TEMPS_EQUIP_ELEC]
+
+def getSortieStockAppElec(annee):
+    return resultats[LIGNE_CONSO_APP_ELEC, annee - TEMPS_APP_ELEC]
+
+def getSortieStock(annee):
+    return getSortieStockVehicules(annee) + getSortieStockBat(annee) + getSortieStockEquipElec(annee) + getSortieStockAppElec(annee)
 
 
-# Les fonctions avant de lancer la simulation
+def getRecyclageSecondaire(annee):
+    '''Donne ce qui va partir en recyclage secondaire et donc revenir dans la production'''
+    pass
 
 
-def initialiser(): #TODO
-    '''
-    Initialise le tableau, avec les données qui peuvent être initialisées avant:
-    - la consommation
-    - les pertes définitivites
-    - ce qui part au recyclage primaire
-    A faire au départ !
-    '''
-    # A FAIRE : on établit la ligne de consommations
-
-    resultats[LIGNE_PERDU_PROD_RAFF] = resultats[LIGNE_CONSO]*POURCENTAGE_PERDU_DEF_RAFFINEMENT_RAPP_CONSO
-    resultats[LIGNE_PERDU_PROD_SEMI_FINISHED] = resultats[LIGNE_CONSO]*POURCENTAGE_PERDU_DEF_SEMI_FINISHED_RAPP_CONSO
-    resultats[LIGNE_RECYCLAGE_PRIMAIRE] = resultats[LIGNE_CONSO]*POURCENTAGE_NEW_WASTE_RAPP_CONSO
+def getAbandonne(annee):
+    '''Donne ce qui sort des stocks mais qui est abandonne'''
+    pass
 
 
+def getNonRecycle(annee):
+    '''Donne ce qui n'est pas recyclé (mauvaise poubelle)'''
+    pass
 
 
+def getPerduRecyclageSecondaire(annee):
+    '''Donne ce qui est perdu lors du recyclage secondaire (rendement)'''
+    pass
 
 
+# Accesseur au tableau :
 
-
-# Les fonctions qui construisent l'année suivante
-
-
-
-
-
-
-
-def doAnneeSuivante():
-    '''
-    Calcule l'année suivante, donc ajoute à chaque ligne concernée un élément de plus
-    Concrètement, on calcule les données de année_actuelle+1 à l'aide des données de année_actuelle
-
-    Modifie le tableau !
-    '''
-    global annee_actuelle
-    global resultats
-    #TODO à compléter avec les autres listes à mettre à jour aussi
-    colonne_stock = calculerStockAnneeSuivante()
-    resultats[0:NB_DONNEES, annee_actuelle - ANNEE_DEBUT]+=colonne_stock
-
-    annee_actuelle+=1
-
+def getRecyclageTotal(annee):
+    '''Donne tout ce qui entre en production par le recyclage (new-waste et old-waste)'''
+    return getRecyclagePrimaire(annee) + getRecyclageSecondaire(annee)
 
 
 def calculerStockAnneeSuivante():
-    '''
-    Donne le stock présent sur le territoire au 1/1/N+1. Ne modifie pas le taleau
+    '''Donne le stock présent sur le territoire au 1/1/N+1. Ne modifie pas le taleau
     Concrètement :
     - récupère le stock au 1/1/N
     - ajoute la consommation pendant l'année N
@@ -151,132 +208,44 @@ def calculerStockAnneeSuivante():
     return stock_prec
 
 
-# Accesseur au tableau :
+# Les fonctions qui construisent l'année suivante
 
+def doAnneeSuivante():
+    '''Calcule l'année suivante, donc ajoute à chaque ligne concernée un élément de plus
+    Concrètement, on calcule les données de année_actuelle+1 à l'aide des données de année_actuelle
 
-def getRecyclageTotal(annee):
-    '''Donne tout ce qui entre en production par le recyclage (new-waste et old-waste)'''
-    return getRecyclagePrimaire(annee) + getRecyclageSecondaire(annee)
-
-
-
-
-
-
-## Accesseurs partie production (tout est initialisé avant, on peut y accéder sans pb)
-
-
-def getBesoin(annee):
+    Modifie le tableau !
     '''
-    Donne le besoin du pays pour l'année, en intégrant à la fois
-    - ce qui sera consommé
-    - ce qui sera perdu définitivement (raffinement + semi-finished goods)
-    - ce qui partira en recyclage primaire (new-waste)
-    Ca correspond à ce qui rentre dans le processus de production
+    global annee_actuelle
+    global resultats
+    #TODO à compléter avec les autres listes à mettre à jour aussi
+    colonne_stock = calculerStockAnneeSuivante()
+    resultats[0:NB_DONNEES, annee_actuelle - ANNEE_DEBUT]+=colonne_stock
+
+    annee_actuelle+=1
+
+
+# Les fonctions avant de lancer la simulation
+
+def initialiser(): #TODO
+    '''Initialise le tableau, avec les données qui peuvent être initialisées avant:
+    - la consommation
+    - les pertes définitivites
+    - ce qui part au recyclage primaire
+    A faire au départ !
     '''
-    return getConsoTotale(annee) + getPerduProductionTotal(annee) + getRecyclagePrimaire(annee)
+    # A FAIRE : on établit la ligne de consommations
+
+    resultats[LIGNE_PERDU_PROD_RAFF] = resultats[LIGNE_CONSO]*POURCENTAGE_PERDU_DEF_RAFFINEMENT_RAPP_CONSO
+    resultats[LIGNE_PERDU_PROD_SEMI_FINISHED] = resultats[LIGNE_CONSO]*POURCENTAGE_PERDU_DEF_SEMI_FINISHED_RAPP_CONSO
+    resultats[LIGNE_RECYCLAGE_PRIMAIRE] = resultats[LIGNE_CONSO]*POURCENTAGE_NEW_WASTE_RAPP_CONSO
 
 
-def getConsoTotale(annee):
-    return getConsoVehicules(annee) + getConsoBat(annee) + getConsoEquipElec(annee) + getConsoAppElec(annee)
+# Programme principal
 
-
-def getConsoVehicules(annee):
-    return resultats[LIGNE_CONSO_VEHICULES, annee_actuelle - ANNEE_DEBUT]
-
-def getConsoBat(annee):
-    return resultats[LIGNE_CONSO_BAT, annee_actuelle - ANNEE_DEBUT]
-
-def getConsoEquipElec(annee):
-    return resultats[LIGNE_CONSO_EQUIP_ELEC, annee_actuelle - ANNEE_DEBUT]
-
-def getConsoAppElec(annee):
-    return resultats[LIGNE_CONSO_APP_ELEC, annee_actuelle - ANNEE_DEBUT]
-
-def getPerduProductionTotal(annee):
-    '''Donne ce qui est perdu définitivement lors de la production (raffinement + semi-finished goods)'''
-    return getPerduProductionRaffinement(annee) + getPerduProductionSemiFinished(annee)
-
-def getPerduProductionRaffinement(annee):
-    '''Donne ce qui est perdu définitivement lors de la partie raffinement de la production'''
-    return resultats[LIGNE_PERDU_PROD_RAFF, annee_actuelle - ANNEE_DEBUT]
-
-def getPerduProductionSemiFinished(annee):
-    '''Donne ce qui est perdu définitivement lors de la production des semi-finished goods'''
-    return resultats[LIGNE_PERDU_PROD_SEMI_FINISHED, annee_actuelle - ANNEE_DEBUT]
-
-def getRecyclagePrimaire(annee):
-    '''Donne ce qui part en recyclage primaire (new waste)'''
-    return resultats[LIGNE_RECYCLAGE_PRIMAIRE, annee_actuelle - ANNEE_DEBUT]
-
-
-
-
-## Accesseurs partie stock (il faut prendre garde à avoir déjà calculé la valeur correspondante !)
-
-def getStock(annee):
-    return getStockVehicules(annee) + getStockBat(annee) + getStockEquipElec(annee) + getStockAppElec(annee)
-
-
-def getStockVehicules(annee):
-    return resultats[LIGNE_STOCK_VEHICULES, annee_actuelle - ANNEE_DEBUT]
-
-def getStockBat(annee):
-    return resultats[LIGNE_STOCK_BAT, annee_actuelle - ANNEE_DEBUT]
-
-def getStockEquipElec(annee):
-    return resultats[LIGNE_STOCK_EQUIP_ELEC, annee_actuelle - ANNEE_DEBUT]
-
-def getStockAppElec(annee):
-    return resultats[LIGNE_STOCK_APP_ELEC, annee_actuelle - ANNEE_DEBUT]
-
-
-
-
-## Accesseurs partie après le stock (à TODO) :
-
-'''
-Donne ce qui sort des stocks:
-- ce qui va être recyclé (fin de vie, old-waste), somme de toutes les catégories
-- ce qui va être perdu définitivement (abandonné, non-recyclé et perdu lors du recyclage)
-'''
-def getSortieStock(annee):
-    return getSortieStockVehicules(annee) + getSortieStockBat(annee) + getSortieStockEquipElec(annee) + getSortieStockAppElec(annee)
-
-
-def getSortieStockVehicules(annee):
-    return resultats[LIGNE_CONSO_VEHICULES, annee - TEMPS_VEHICULES]
-
-def getSortieStockBat(annee):
-    return resultats[LIGNE_CONSO_BAT, annee - TEMPS_BAT]
-
-def getSortieStockEquipElec(annee):
-    return resultats[LIGNE_CONSO_EQUIP_ELEC, annee - TEMPS_EQUIP_ELEC]
-
-def getSortieStockAppElec(annee):
-    return resultats[LIGNE_CONSO_APP_ELEC, annee - TEMPS_APP_ELEC]
-
-
-
-
-
-def getRecyclageSecondaire(annee):
-    '''Donne ce qui va partir en recyclage secondaire et donc revenir dans la production'''
-    pass
-
-
-
-def getAbandonne(annee):
-    '''Donne ce qui sort des stocks mais qui est abandonne'''
-    pass
-
-
-def getNonRecycle(annee):
-    '''Donne ce qui n'est pas recyclé (mauvaise poubelle)'''
-    pass
-
-
-
-def getPerduRecyclageSecondaire(annee):
-    '''Donne ce qui est perdu lors du recyclage secondaire (rendement)'''
-    pass
+def simul():
+    '''Réalise la simulation'''
+    initialiser()
+    while annee_actuelle < ANNEE_FIN:
+        doAnneeSuivante()
+    #TODO
