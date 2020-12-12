55ffb9b971c526cdeef588b23f821f5958711d16
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-12 21:35:22 +0100

    Premier dessin : histogramme avec les catégories pour les durées de vie

--- dessins_pour_poster.py
+++ dessins_pour_poster.py
@@ -10,3 +10,10 @@ liste_ages = np.array([10, 50, 10, 3])
 liste_stocks = [1_000_000, 20_000_000,  225_000,10_000]
 liste_conso = [200_000, 200_000, 20_000,2_500]
 
+## Premier dessin : histogramme avec les catégories pour les durées de vie
+
+
+plt.bar(["Véhicules", "Bâtiments", "Equipements\nélectroménagers", "Appareils\nélectroniques" ],
+        liste_ages)
+plt.xticks(rotation=45)
+# plt.show()
