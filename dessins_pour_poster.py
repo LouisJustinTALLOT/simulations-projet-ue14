6261d80f472905f30ec454611305a0a68a604bfb
Louis-Justin TALLOT <lj.tallot@gmail.com>
2020-12-12 21:35:11 +0100

    les données

--- dessins_pour_poster.py
+++ dessins_pour_poster.py
@@ -1,3 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+## les données
+
+#              0         1                   2                          3 
+#       cat = Véhicules Batiments Equipements_electroménagers Appareils_électroniques
+
+liste_ages = np.array([10, 50, 10, 3])
+liste_stocks = [1_000_000, 20_000_000,  225_000,10_000]
+liste_conso = [200_000, 200_000, 20_000,2_500]
+
